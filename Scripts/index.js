dc06b8585e25d4facf512ed57a7a8a4829ae14a4
JaniHegedus <90450741+JaniHegedus@users.noreply.github.com>
2022-06-19 18:18:31 +0200

    Youtube prev Page

---
+++ Scripts/index.js
@@ -0,0 +1,61 @@
+
+function alapok()
+{
+  console.log("Hello Buzz!");
+  let mosh= "mosh";
+  console.log(mosh);
+  let person = {
+    name:"Jani",
+    age: 19
+  };
+  console.log(person)
+  person.age=32;
+  console.log("13 years after: \n");
+  console.log(person);
+  let selected="name";
+  person[selected]="Korbusz";
+  let selectedcolors= ["red","blue"];
+  console.log("A színek száma: "+selectedcolors.length);
+  selectedcolors[2]="green";
+  selectedcolors[3]=2;
+  console.log(square(3));
+}
+function arged(name)
+{
+  console.log("Hello "+name);
+}
+function square(number)
+{
+  return number*number;
+}
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState === 4) {
+          if (this.status === 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status === 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+alapok();
+arged("John");
+includeHTML();
